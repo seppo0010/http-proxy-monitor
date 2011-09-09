5aaf2525ec616603d8bed788a363345a96fb8e99
Sebastian Waisbrot <seppo0010@gmail.com>
2011-09-09 10:03:51 -0300

    Consistent whitespacE

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.config = {
-	port: 8080,
-	interactive: true,
+    port: 8080,
+    interactive: true,
     buffer_size: 1024,
     throttling: 1,
     database: {
--- main.js
+++ main.js
@@ -5,16 +5,16 @@ var mysql = require('mysql');
 var database;
 var allowed_ips = ['127.0.0.1'];
 var server = http.createServer(function(request, response) {
-	if (allowed_ips.indexOf(request.connection.remoteAddress) == -1) {
-		// TODO: dynamic IP whitelisting
-		response.writeHead(401);
-		response.write("Dynamic IP allowing is pending");
-		response.end();
-	}
-	var host = request.headers['host'].split(':');
-	var proxy = http.createClient(host[1] || 80, host[0])
+    if (allowed_ips.indexOf(request.connection.remoteAddress) == -1) {
+        // TODO: dynamic IP whitelisting
+        response.writeHead(401);
+        response.write("Dynamic IP allowing is pending");
+        response.end();
+    }
+    var host = request.headers['host'].split(':');
+    var proxy = http.createClient(host[1] || 80, host[0])
     console.log('Requesting ' + request.url);
-	var proxy_request = proxy.request(request.method, request.url.substr(request.headers['host'].length + 7), request.headers);
+    var proxy_request = proxy.request(request.method, request.url.substr(request.headers['host'].length + 7), request.headers);
     var requestId = 0;
     database.query('INSERT INTO request (request_url, request_method, request_headers, request_body, response_headers, response_body) VALUES (?, ?, ?, ?, "", "")', [
             request.url,
@@ -25,7 +25,7 @@ var server = http.createServer(function(request, response) {
             requestId = (info.insertId);
         }
     );
-	proxy_request.addListener('response', function(proxy_response) {
+    proxy_request.addListener('response', function(proxy_response) {
         // TODO: fix race condition with DB insert id
         database.query('UPDATE request SET response_code = ?, response_headers = ? WHERE id = ?', [
             proxy_response.statusCode,
@@ -48,29 +48,29 @@ var server = http.createServer(function(request, response) {
                 clearInterval(timer);
             }
         }, config.throttling);
-		proxy_response.addListener('data', function(chunk) {
+        proxy_response.addListener('data', function(chunk) {
             var newBuffer = new Buffer(buffer.length + chunk.length);
             buffer.copy(newBuffer);
             chunk.copy(newBuffer, buffer.length);
             buffer = newBuffer;
-		});
-		proxy_response.addListener('end', function() {
+        });
+        proxy_response.addListener('end', function() {
             // TODO: fix race condition with DB insert id
             database.query('UPDATE request SET response_body = ? WHERE id = ?', [
                 buffer.toString('utf-8'),
                 requestId,
             ]);
             ended = true;
-		});
-		response.writeHead(proxy_response.statusCode, proxy_response.headers);
-	});
-	request.addListener('data', function(chunk) {
+        });
+        response.writeHead(proxy_response.statusCode, proxy_response.headers);
+    });
+    request.addListener('data', function(chunk) {
         // TODO: log request body
-		proxy_request.write(chunk, 'binary');
-	});
-	request.addListener('end', function() {
-		proxy_request.end();
-	});
+        proxy_request.write(chunk, 'binary');
+    });
+    request.addListener('end', function() {
+        proxy_request.end();
+    });
 });
 
 server.listen(config.port);
